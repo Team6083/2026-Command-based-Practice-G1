f7d72bfebce4eb4e4944f901e7943779fa074195
way_x112 <j110102@my.cmsh.cyc.edu.tw>
2025-12-31 20:17:40 +0800

    fix

--- src/main/java/frc/robot/subsystems/TankSubsystem.java
+++ src/main/java/frc/robot/subsystems/TankSubsystem.java
@@ -4,10 +4,10 @@
 
 package frc.robot.subsystems;
 
-import frc.robot.Constants.TankConstants;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.TankConstants;
 
 public class TankSubsystem extends SubsystemBase {
 
