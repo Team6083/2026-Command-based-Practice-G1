bbdc197708524b84e8f7e55dfbaeff499510a678
way_x112 <j110102@my.cmsh.cyc.edu.tw>
2026-01-02 19:25:19 +0800

    Add Climber subsystem and command; integrate with RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,8 +8,10 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.XboxController;
+import frc.robot.commands.ClimberCmd;
 import frc.robot.commands.CoralShooterCmd;
 import frc.robot.commands.TankCmd;
+import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.CoralShooterSubsystem;
 import frc.robot.subsystems.TankSubsystem;
 
@@ -18,17 +20,20 @@ public class RobotContainer {
   TankSubsystem tanksubsystem;
   CoralShooterSubsystem coralShooterSubsystem;
   CommandXboxController controller;
+  ClimberSubsystem climberSubsystem;
 
   public RobotContainer() {
     controller = new CommandXboxController(XboxController.XboxControllerPort);
     tanksubsystem = new TankSubsystem();
     coralShooterSubsystem = new CoralShooterSubsystem();
+    climberSubsystem = new ClimberSubsystem();
     configureBindings();
   }
 
   private void configureBindings() {
     tanksubsystem.setDefaultCommand(new TankCmd(tanksubsystem, controller));
     coralShooterSubsystem.setDefaultCommand(new CoralShooterCmd(coralShooterSubsystem, controller));
+    controller.a().onTrue(new ClimberCmd(climberSubsystem));
   }
 
   public Command getAutonomousCommand() {
